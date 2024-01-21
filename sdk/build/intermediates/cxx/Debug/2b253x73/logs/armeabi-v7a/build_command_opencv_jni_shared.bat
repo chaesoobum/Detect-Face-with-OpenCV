a0ac743cb9f6ee31ae0337278ce22fa57f7c5faa
soobu <soobum007@naver.com>
2024-01-22 01:24:36 +0900

    Initial commit

---
+++ sdk/build/intermediates/cxx/Debug/2b253x73/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\soobu\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\android2\\UseOpenCVwithCMake\\sdk\\.cxx\\Debug\\2b253x73\\armeabi-v7a" ^
+  opencv_jni_shared
---
+++ sdk/build/intermediates/cxx/Debug/2b253x73/logs/x86/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\soobu\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\android2\\UseOpenCVwithCMake\\sdk\\.cxx\\Debug\\2b253x73\\x86" ^
+  opencv_jni_shared
